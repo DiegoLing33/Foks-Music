6ceb3c8f87fbb1187abea68db2c37eea26ae71f2
Яков Панов <diegoling33@yandex.ru>
2021-10-20 14:57:05 +0300

    Initial Commit

---
+++ Foks Guitar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Foks Guitar
+//
+//  Created by Яков Линг on 20.10.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
