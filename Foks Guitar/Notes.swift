66fc2fe68cfb2354e9973a7c98f03d18ac21af12
Яков Панов <diegoling33@yandex.ru>
2021-10-22 05:04:32 +0300

    Added some entities

--- Foks Guitar/Notes.swift
+++ Foks Guitar/Notes.swift
@@ -23,3 +23,68 @@ let nB = 11
 let NotesList = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 let Chords = ["C", "Cm", "C#", "C#m", "D", "Dm", "D#", "D#m", "E", "Em", "F", "Fm", "F#", "F#m", "G", "Gm", "G#", "G#m", "A", "Am", "A#", "A#m", "B", "Bm"]
+
+enum NoteKey: String {
+    case C = "C", D = "D", E = "E", F = "F", G = "G", A = "A", B = "B"
+}
+
+enum ChordModifier: String {
+    case minor = "m",
+         major = ""
+}
+
+/// Нота
+struct Note: RawRepresentable {
+    
+    
+    // Notes
+    static let C = Note(.C)
+    
+    // Class
+    
+    typealias RawValue = String
+    
+    var rawValue: String {
+        get { return "\(key.rawValue.uppercased())\(sharp ? "#" : "")"}
+    }
+    
+    var key: NoteKey
+    var sharp: Bool
+    
+    init(_ key: NoteKey, sharp: Bool = false){
+        self.key = key
+        self.sharp = sharp
+    }
+    
+    init?(rawValue: String) {
+        self.key = NoteKey(rawValue: rawValue.first?.uppercased() ?? "C") ?? .C
+        self.sharp = rawValue.contains("#")
+    }
+}
+
+
+/// Аккорд
+struct Chord: RawRepresentable {
+    // class
+    
+    typealias RawValue = String
+    
+    let note: Note
+    let modifier: ChordModifier
+    
+    var rawValue: String {
+        get { return "\(note.rawValue)\(modifier.rawValue)" }
+    }
+    
+    /// Создает аккорд из исходного значения
+    init?(rawValue: String) {
+        self.note = Note(rawValue: rawValue.prefix(2).uppercased()) ?? .C
+        self.modifier = ChordModifier(rawValue: rawValue.suffix(1).lowercased()) ?? .major
+    }
+    
+    
+    init(_ note: Note, modifier: ChordModifier = .major){
+        self.note = note
+        self.modifier = modifier
+    }
+}
